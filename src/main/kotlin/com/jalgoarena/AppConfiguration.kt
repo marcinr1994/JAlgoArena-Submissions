781503944c1d93d9512b39ab09616bb4f1c26622
marcinrejdych <marcin.rejdych@relayr.io>
2018-01-21 00:05:49 +0100

    mr: added swagger, changes in build.gradle, dockerfiles, docker-compose and configuration

--- src/main/kotlin/com/jalgoarena/AppConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/AppConfiguration.kt
@@ -7,8 +7,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestOperations
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
+@EnableSwagger2
 open class AppConfiguration {
 
     @Bean
@@ -23,4 +29,12 @@ open class AppConfiguration {
 
     @Bean
     open fun restTemplate(): RestOperations = RestTemplate()
+
+
+    @Bean
+    open fun jalGoArenaApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.jalgoarena"))
+                .paths(PathSelectors.any()).build()
+    }
 }
